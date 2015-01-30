fd778fdd9989f7373e2086fa979907c73f526567
Scully87 <scully87.ts@googlemail.com>
2015-01-30 14:48:30 +0000

    uses method to return a string

--- spec/pubNamerSpec.js
+++ spec/pubNamerSpec.js
@@ -1,23 +1,23 @@
 describe('pubNamer', function(){
 
+  beforeEach (function() {
+    pubnamer = new pubNamer();
+  });
+
   it('should pluralize a noun ending in S', function(){
-    var pubnamer = new pubNamer();
     expect(pubnamer.pluralize('lass')).toEqual('lasses');
   });
 
   it('should pluralize a noun not ending in S', function(){
-    var pubnamer = new pubNamer();
     expect(pubnamer.pluralize('stag')).toEqual('stags');
   });
 
-  it('should generate two random nouns for the "The noun and noun" formula', function(){
-    var pubnamer = new pubNamer();
+  xit('should generate two random nouns for the "The noun and noun" formula', function(){
     expect(pubnamer.generate('nounandnoun')).toEqual('the duck and goose');
   });
 
   it('should return a random element of an array', function(){
-    var pubnamer = new pubNamer();
-    expect(pubnamer.getRandomWord(nouns)).toEqual('dog');
+    expect(pubnamer.getRandomWord(nouns)).toEqual(jasmine.any(String));
   });
 
 });
