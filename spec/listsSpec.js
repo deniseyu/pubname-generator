5079565fcaea5152ec6e0fd29abf291df6a14704
Spike Nox <spike01@gmail.com>
2015-02-03 10:39:35 +0000

    Rebases from master, adding in new words, corrects tests

--- spec/listsSpec.js
+++ spec/listsSpec.js
@@ -5,9 +5,7 @@ describe('Lists', function() {
   });
 
   it('selects a random element of a given list', function() {
-    //in this case, Math.random being set to 0.1 always returns 6, so the 5th array
-    //element
-    expect(lists.random('noun')).toEqual('Boot');
+    expect(lists.random('noun')).toEqual('Bicycle');
   });
 });
 
